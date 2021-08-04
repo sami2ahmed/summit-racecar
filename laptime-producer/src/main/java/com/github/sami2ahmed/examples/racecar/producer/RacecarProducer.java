349bdab79f94160caf9ea9f32bf5b8cc8ca92f20
sami2ahmed <samiahmedkeys@gmail.com>
2021-08-04 15:32:02 -0500

    yaml small changes

--- laptime-producer/src/main/java/com/github/sami2ahmed/examples/racecar/producer/RacecarProducer.java
+++ laptime-producer/src/main/java/com/github/sami2ahmed/examples/racecar/producer/RacecarProducer.java
@@ -90,7 +90,7 @@ public class RacecarProducer {
     }
 
     boolean isRaceStatus = false;
-    @Scheduled(fixedDelay = 10L)
+    @Scheduled(fixedDelay = 100L)
     private void publishRacecarRecord() throws JsonProcessingException {
         if(this.records.hasNext()) {
             RaceDetails raceDetails = ImmutableRaceDetails.builder()
--- race-car-events/src/main/java/com/github/sami2ahmed/examples/racecar/demo;/RaceCarEventsApplication.java
+++ race-car-events/src/main/java/com/github/sami2ahmed/examples/racecar/demo;/RaceCarEventsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.racecarevents.controller"})
 public class RaceCarEventsApplication {
 
 	public static void main(String[] args) {
