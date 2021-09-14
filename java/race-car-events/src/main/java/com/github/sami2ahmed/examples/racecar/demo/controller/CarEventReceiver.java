aa702f9dfae2ab28f52b48d2dc4c31917724d5b2
sami2ahmed <samiahmedkeys@gmail.com>
2021-09-14 18:38:38 -0500

    updated config in CarEventRececiver

--- java/race-car-events/src/main/java/com/github/sami2ahmed/examples/racecar/demo/controller/CarEventReceiver.java
+++ java/race-car-events/src/main/java/com/github/sami2ahmed/examples/racecar/demo/controller/CarEventReceiver.java
@@ -7,6 +7,8 @@ import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -28,59 +30,62 @@ import java.util.UUID;
 @RestController
 @RequestMapping(path = "/v1/api")
 public class CarEventReceiver {
-
+    private static final Logger log = LoggerFactory.getLogger(CarEventReceiver.class);
     private static final String RACE_CAR_TOPIC = "racecarDemo";
     private static final String ODDS_LANDING = "odds-landing";
 
     @Value("${spring.kafka.bootstrap-servers}")
-    private String BOOTSTRAP_SERVERS = "pkc-ep9mm.us-east-2.aws.confluent.cloud:9092";
+    private String BOOTSTRAP_SERVERS = "";
 
     @Value("${spring.kafka.security.protocol}")
-    private String SECURITY_PROTOCOL = "SASL_SSL";
+    private String SECURITY_PROTOCOL = "";
 
     @Value("${spring.kafka.properties.sasl.mechanism}")
-    private String saslMechanism = "PLAIN";
+    private String saslMechanism = "";
 
     @Value("${spring.kafka.properties.sasl.jaas.config}")
-    private String jaasConfig = "org.apache.kafka.common.security.plain.PlainLoginModule   required username='YGGQLP4YEP4KGH3R'   password='oD5cgmrBKDFLK1+nlOVC+NFeYZ9BpHDcsnQuhdgJeMaQhlHChlw/5+Cgsgl4lvni';";
+    private String jaasConfig = "";
+
 
     @GetMapping(value = "/odds/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Odds> getOdds(){
+        log.info("getOdds()");
         Map<String, Object> propsMaps = this.kafkaReceiverConfigurations("odds");
 
 
         Deserializer<Odds> deserializer = new JsonDeserializer<Odds>();
         deserializer.configure(propsMaps, false);
         ReceiverOptions<String, Odds> receiverOptions = ReceiverOptions.<String, Odds>create(propsMaps)
-            .withValueDeserializer(deserializer)
-            .withKeyDeserializer(new StringDeserializer())
-            .subscription(Collections.singletonList(ODDS_LANDING));
+                .withValueDeserializer(deserializer)
+                .withKeyDeserializer(new StringDeserializer())
+                .subscription(Collections.singletonList(ODDS_LANDING));
 
         DefaultKafkaReceiver<String, Odds> kafkaReceiver =
-            new DefaultKafkaReceiver<>(ConsumerFactory.INSTANCE, receiverOptions);
+                new DefaultKafkaReceiver<>(ConsumerFactory.INSTANCE, receiverOptions);
 
         Flux<ReceiverRecord<String, Odds>> kafkaFlux = kafkaReceiver.receive();
 
         return kafkaFlux
-            .map(ReceiverRecord::value).log();
+                .map(ReceiverRecord::value).log();
 
     }
 
 
     @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<LapTime> getOrdersEventsFlux(@RequestParam(name = "raceId") String driverId){
+        log.info("getOrdersEventsFlux() driverId = '{}'", driverId);
         Map<String, Object> propsMaps = this.kafkaReceiverConfigurations(driverId);
 
 
         Deserializer<LapTime> deserializer = new JsonDeserializer<LapTime>();
         deserializer.configure(propsMaps, false);
         ReceiverOptions<String, LapTime> receiverOptions = ReceiverOptions.<String, LapTime>create(propsMaps)
-            .withValueDeserializer(deserializer)
-            .withKeyDeserializer(new StringDeserializer())
-            .subscription(Collections.singletonList(RACE_CAR_TOPIC));
+                .withValueDeserializer(deserializer)
+                .withKeyDeserializer(new StringDeserializer())
+                .subscription(Collections.singletonList(RACE_CAR_TOPIC));
 
         DefaultKafkaReceiver<String, LapTime> kafkaReceiver =
-            new DefaultKafkaReceiver<>(ConsumerFactory.INSTANCE, receiverOptions);
+                new DefaultKafkaReceiver<>(ConsumerFactory.INSTANCE, receiverOptions);
 
         Flux<ReceiverRecord<String, LapTime>> kafkaFlux = kafkaReceiver.receive();
 
@@ -99,13 +104,10 @@ public class CarEventReceiver {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.CLIENT_ID_CONFIG, "car-consumer-"+id+"-"+ UUID.randomUUID());
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, id);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        //props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, JsonNode.class);
         props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SECURITY_PROTOCOL);
         props.put("sasl.mechanism",saslMechanism);
