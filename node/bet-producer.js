a0c5f3e23f792b7bfd6b3b633e2fdb2d83d6d26c
Amanda Gilbert <amanda.gilbert2014@gmail.com>
2021-09-14 17:24:31 -0400

    Create bet-producer.js
    bet-producer for simulating bets

---
+++ node/bet-producer.js
@@ -0,0 +1,67 @@
+/*
+should be run like so: 
+node bet-producer.js <bootstrap-server> <api-key> <api-secret> <driver_id of driver you want to saturate with bets> <# of bets to saturate> 
+ie: node bet-producer.js <bootstrap-server> <api-key> <api-secret> 4 400
+*/
+var myArgs = process.argv.slice(2);
+
+const { Kafka } = require('kafkajs')
+const jsf = require('json-schema-faker');
+jsf.extend('faker', () => require('faker'));
+const conf = {
+    clientId: 'bet-producer',
+    brokers: [myArgs[0]],
+    ssl: true,
+    sasl: {
+      mechanism: 'plain',
+      username: myArgs[1],
+      password: myArgs[2],
+    },
+  }
+
+const kafka = new Kafka(conf)
+const producer = kafka.producer()
+
+jsf.option({useExamplesValue: true})
+
+var sendMessages = async() => {
+  await producer.connect()
+  for (let i=0; i < myArgs[4]; i++){
+    const bet = {
+      "type": "object",
+      "required": ["betId", "userId", "driverId", "betAmt"],
+      properties: {
+        betId: {
+          type: 'integer',
+          unique: true,
+          minimum: 0,
+          exclusiveMinimum: true
+        },
+        userId: {
+          type: "integer",
+          minimum: 1,
+          maximum: 100000
+        },
+        driverId: {
+          type: "integer",
+          examples: [myArgs[3]]
+        },
+        betAmt: {
+          type: "integer",
+          minimum: 1,
+          maximum: 100
+        }
+      }
+    }
+    console.log(jsf.generate(bet)["betId"])
+    await producer.send({
+      topic: 'bet-landing',
+      messages: [
+        {key: (jsf.generate(bet)["betId"]).toString(), value: JSON.stringify(jsf.generate(bet))}
+      ]
+    })
+  }
+  await producer.disconnect()
+}
+
+sendMessages();
