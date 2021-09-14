ddebe98487b09c78a9fcff03834497f9e865e3c5
sami2ahmed <samiahmedkeys@gmail.com>
2021-09-14 17:18:43 -0500

    updated bash sse file

--- sse.sh
+++ sse.sh
@@ -1,4 +1,4 @@
-cd backend/race-car-events
+cd java/race-car-events
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk-11.0.8.jdk/Contents/Home'
 mvn package -U
 mvn org.springframework.boot:spring-boot-maven-plugin:run
