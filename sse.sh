568bcd011d9ba0cf192ae174c953083b8590bacc
Sami Ahmed <samiahmedkeys@gmail.com>
2021-09-15 12:25:21 -0500

    Update sse.sh

--- sse.sh
+++ sse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+cd java
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk-11.0.8.jdk/Contents/Home'
 mvn install
 cd java/race-car-events
-mvn org.springframework.boot:spring-boot-maven-plugin:run
